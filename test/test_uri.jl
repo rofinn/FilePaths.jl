2dbb65562db757fe211156d6c888a90784ffc35d
David Anthoff <anthoff@berkeley.edu>
2018-05-09 11:59:33 -0700

    Add two URI tests

--- test/test_uri.jl
+++ test/test_uri.jl
@@ -4,4 +4,6 @@ using FilePaths
 @testset "URI" begin
     @test string(URI(p"/foo/bar")) == "file:///foo/bar"
     @test string(URI(p"/foo foo/bar")) == "file:///foo%20foo/bar"
+    @test_throws ArgumentError URI(p"foo/bar")
+    @test string(URI(WindowsPath("C:\\foo\\bar"))) == "file:///C:/foo/bar"
 end
