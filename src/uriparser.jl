aa8e58285a6704c5eaa583ba0211a22196ffaed1
John M Kuhn <John.M.Kuhn@noaa.gov>
2021-04-22 22:32:24 -0400

    Test both URIParser and URIs

--- src/uriparser.jl
+++ src/uriparser.jl
@@ -1,8 +1,7 @@
 using .URIParser
 
-Base.depwarn("`URIParser` is deprecated, use `URIs` instead.", :URIParser, force=true)
-
 function URIParser.URI(p::AbstractPath; query="", fragment="")
+    Base.depwarn("`URIParser` is deprecated, use `URIs` instead.", :URIParser)
     if isempty(p.root)
         throw(ArgumentError("$p is not an absolute path"))
     end
